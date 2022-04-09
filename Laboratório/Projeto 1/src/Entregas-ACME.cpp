27a9d5fcdb84c3d8acb313297611367666cbbeee
Lucas Teles <ls5651405@gmail.com>
2022-04-09 11:58:42 -0300

    feat: adds test case for use in input

---
+++ Laboratório/Projeto 1/src/Entregas-ACME.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+#include "Dijkstra.cpp"
+
+using namespace std;
+
+struct entrega {
+    int local;
+    int peso;
+};
+
+int main() {
+
+    int qtdLocais;
+    int qtdEntregas;
+    int pesoMaximoEntregador;
+
+
+
+    return 0;
+}
